7ee5f34d43fc5947e992993686d3867125f9cd2d
Ryan <snapsglasen@gmail.com>
2022-05-24 12:16:27 +0900

    complete objective2lab2

---
+++ labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Ryan Palmer";
+    int age = 30;
+    String state = "Maryland";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+    }
+}
