a72ba3eef3e412d2944ac6fd993df72192a6fdaa
Ryan <snapsglasen@gmail.com>
2022-05-24 12:19:30 +0900

    complete objective3lab3

---
+++ labs/Objective3Lab2.java
@@ -0,0 +1,10 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1, side2, hypotenuse;
+    side1 = 2;
+    side2 = 3;
+    hypotenuse = (side1 * side1) + (side2 + side2);
+
+    System.out.println(hypotenuse);
+  }
+}
