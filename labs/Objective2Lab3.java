8b55ca3668dfe33a228ed3d15c5281713f2c0161
Ryan <snapsglasen@gmail.com>
2022-05-24 12:17:25 +0900

    complete objective2lab3

---
+++ labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args){
+
+  int num1 = 15;
+  int num2 = 6;
+  int num3 = 2;
+  double average = (num1 + num2 + num3)/3;
+
+  System.out.println(average);
+  }
+}
