eb8aeb8a8dd162d4d89b3f832d13c0d1aa62d626
Ryan <snapsglasen@gmail.com>
2022-05-25 16:20:41 +0900

    completeobjective5lab1

---
+++ labs/Objective5Lab1.java
@@ -0,0 +1,14 @@
+public class Objective5Lab1 {
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 15;
+
+    if (num1 > num2) {
+      System.out.println("Wrong.");
+
+    }
+    else if (num2 > num1) {
+      System.out.println("15 is greater than 5.");
+    }
+  }
+}
