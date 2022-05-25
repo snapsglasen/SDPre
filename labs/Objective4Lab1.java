c691e6717dc71dabd0efb2ce18e2711ee22ac05f
Ryan <snapsglasen@gmail.com>
2022-05-25 11:08:02 +0900

    completeobjective4lab1

---
+++ labs/Objective4Lab1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Objective4Lab1 {
+  public static void main(String[] args) {
+
+    Scanner keyboard = new Scanner(System.in);
+
+  
+    System.out.println("What is your first name?");
+    String fname = keyboard.nextLine();
+
+    System.out.println("What is your last name?");
+    String lname = keyboard.nextLine();
+
+    System.out.println("What is your favorite food?");
+    String favoriteFood = keyboard.nextLine();
+
+    System.out.println("What is your favorite animal?");
+    String favoriteAnimal = keyboard.nextLine();
+
+    System.out.println("What is your favorite song?");
+    String favoriteSong = keyboard.nextLine();
+
+
+
+  }
+}
