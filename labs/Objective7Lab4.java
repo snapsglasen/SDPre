e18293006e2356056a734dbbb637ad7f821659fb
Ryan <snapsglasen@gmail.com>
2022-05-26 15:43:52 +0900

    complete objective7lab4

---
+++ labs/Objective7Lab4.java
@@ -0,0 +1,17 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+
+    int counter = 1;
+    int sum = 0;
+
+      do {
+        sum += counter;
+        counter ++;
+      }
+      while(counter <= 20);
+
+     System.out.println(sum);
+
+
+    }
+  }
