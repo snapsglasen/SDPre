4aea603a5896e17c6af1c154e8d51438cbe21114
Ryan <snapsglasen@gmail.com>
2022-05-26 16:30:18 +0900

    complete objective8lab1

---
+++ labs/Objective8Lab1.java
@@ -0,0 +1,13 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    for (int num = 0; num <= 10; num += 1) {
+      System.out.println(num);
+    }
+    for (int num = 10; num >= 0; num -= 1) {
+      System.out.println(num);
+    }
+  }
+
+
+}
