8becc4eacd0cc79bafd564811d71a28ff25ef20b
Ryan <snapsglasen@gmail.com>
2022-05-27 12:48:16 +0900

    complete objective8lab2

---
+++ labs/Objective8Lab2.java
@@ -0,0 +1,12 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int sum = 0;
+
+
+    for (int counter = 0; counter <= 20; counter ++){
+    sum = sum + counter;
+
+    }
+    System.out.println(sum);
+  }
+}
